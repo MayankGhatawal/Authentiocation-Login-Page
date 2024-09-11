5593272ad4634913809bda1453df60d98c12a23e
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-12 00:20:25 +0530

    main

--- Backend/Routes/auth.route.js
+++ Backend/Routes/auth.route.js
@@ -1,18 +1,11 @@
 import express from 'express';
+import { login, logout, signup } from '../controller/auth.controller';
 
 const router = express.Router();
 
-router.get('/signup', (req, res) => {
-  res.send('Signup routes are available');
-});
-
-router.get('/login', (req, res) => {
-  res.send('Login routes are available');
-});
-
-router.get('/logout', (req, res) => {
-  res.send('Logout routes are available');
-});  
+router.get('/signup', signup);
+router.get('/login', login);
+router.get('/logout', logout);  
 
 
 
