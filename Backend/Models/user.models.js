be160a3bd67a692d0fe705471186cce551b22dd0
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-12 00:27:09 +0530

    main

---
+++ Backend/Models/user.models.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const Users = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: String,
+    resetPasswordTokenExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiresAt: Date
+}, {timestamps: true})
+
+export default mongoose.model('Users', Users);
