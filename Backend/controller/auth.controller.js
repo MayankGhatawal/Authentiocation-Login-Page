ab3675e85e30055bf694ddc58df11e248f24dbcf
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-12 00:43:36 +0530

    main

--- Backend/controller/auth.controller.js
+++ Backend/controller/auth.controller.js
@@ -1,5 +1,22 @@
+import { User } from '../Models/user.models.js'
+import bcrypt from 'bcrypt';
+
 export const signup = async (req, res) => {
-    res.send("Signup Routes");
+    const { email, name, password } = req.body;
+    try {
+        if(!email || !name || !password){
+            throw new Error('All Fields must be Required');
+        }
+
+        const userAlreadyExists = await User.findOne({email});
+        if(userAlreadyExists) {
+            return res.status(400).json({success:false ,message: 'User already exists'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10)
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
 }
 
 export const login = async (req, res) => {
