094e230cdac82810e5bb44af0e2a93b780f455b7
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-09-12 00:20:06 +0530

    main

---
+++ Backend/controller/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+    res.send("Signup Routes");
+}
+
+export const login = async (req, res) => {
+    res.send("Login Routes");
+}
+
+export const logout = async (req, res) => {
+    res.send("Logout Routes");
+}
